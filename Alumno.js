d37c37fc5a7c30223ce589028a138ca0c5720a1f
You <abril.barrientos@davinci.edu.ar>
2025-08-12 10:02:38 -0300

    ARREGLAR Y TERMINAR

--- Alumno.js
+++ Alumno.js
@@ -1,9 +1,40 @@
 class Alumno {
-    //atributos
-    nombre: "Yo",
-    apellido:"Yoyo",
-    edad: 21,
-    carrera:"DW",
-    materia:"Aplicaciones Híbridas"
-    //métodos
+    // Atributos
+    nombre = "";
+    apellido = ""
+    edad;
+    carrera = "";
+
+    constructor(nombre, apellido, edad, carrera, materias=[]){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.materias = materias;
+    }
+
+    // Métodos
+    obtenerCarrera(){
+        return this.carrera;
+    }
+    obtenerDatos(){
+        return `${this.nombre} ${this.apellido}`; 
+    }
+    modificarEdad(edad){
+        this.edad = edad;
+    }
+
+    agregarMateria(materia){
+        return materias.push(materia)
+    }
+
+    mostrarMaterias(){
+        return this.materias;
+    }
+
+    retornarEdad(){
+        return this.edad;
+    }
 }
+let algo = "texto";
+module.exports = Alumno;
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let nombre = "Yo";
-console.log("Holi " + nombre);
+const Alumno =require('./Alumno.js');
+
+let nombre = "Jonathan";
+
+const alumno1 = new Alumno (nombre, "Ruiz", 27, "DW" ,"pintar");
+alumno1.modificarEdad(35);
+
+alumno1.agregarMateria("deontología")
+
+console.log( alumno1.edad);
+
+console.log(alumno1.materias)
